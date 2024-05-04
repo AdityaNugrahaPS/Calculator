6695cdb6944de25a2513d949a5b4dc701e595660
AdityaNugrahaPS <adityanugrahasaiya@gmail.com>
2024-05-04 22:57:36 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// Calculator Program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) { //menampilkan inputan ke display
+    display.value += input
+}
+
+function clearDisplay(){
+    display.value = ''
+}
+
+function calculate() { //meghitung hasil
+    try {
+        display.value = eval(display.value)
+    } catch(error) {
+        display.value = 'error'
+    }
+
+}
